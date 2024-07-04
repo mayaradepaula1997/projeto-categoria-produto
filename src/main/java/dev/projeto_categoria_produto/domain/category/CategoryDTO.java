912f72b0061e706680972e8b7a9b0b4de064ef2a
Mayara <mayaradepaula1997>
2024-07-04 18:44:35 -0300

    git commit -m " ProductDTO and CategoryDTO"

---
+++ src/main/java/dev/projeto_categoria_produto/domain/category/CategoryDTO.java
@@ -0,0 +1,4 @@
+package dev.projeto_categoria_produto.domain.category;
+
+public record CategoryDTO(Long id,String title, String description) {
+}
---
+++ src/main/java/dev/projeto_categoria_produto/domain/product/ProductDTO.java
@@ -0,0 +1,4 @@
+package dev.projeto_categoria_produto.domain.product;
+
+public record ProductDTO(String title, String description, Integer price,String categoryId) {
+}
